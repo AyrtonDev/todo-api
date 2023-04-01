683d24a9ad8fdc9192d423fdd7cefcbd902149c2
Ayrton <ayrton.guerra@cliqx.com.br>
2023-04-01 11:24:03 -0300

    initial app todo list

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req: Request, res: Response) => {
+  res.status(404);
+  res.json({
+    error: "Endpoint not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
